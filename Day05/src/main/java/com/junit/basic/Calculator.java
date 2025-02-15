ea88299f6010667f7947845285b796b917c9c850
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-02-15 16:42:55 +0530

    Adarsh gupta - Calculator

---
+++ Day05/src/main/java/com/junit/basic/Calculator.java
@@ -0,0 +1,37 @@
+//package
+package com.junit.basic;
+
+//class
+public class Calculator {
+
+    //addition method
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    //subtraction method
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+
+    //multiplication method
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+
+    //division method
+    public static double divide(int a, int b){
+
+        double answer = 0;
+
+        try {
+            answer = a/b;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Division by 0 not allowed");
+            System.out.println(e.getMessage());
+        }
+        return answer ;
+    }
+
+}
