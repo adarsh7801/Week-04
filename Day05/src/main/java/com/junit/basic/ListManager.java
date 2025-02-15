6163632bbc2ff067d4a8a1a484c911cc09430e5f
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-02-15 16:44:10 +0530

    Adarsh gupta - List Manager

---
+++ Day05/src/main/java/com/junit/basic/ListManager.java
@@ -0,0 +1,36 @@
+//package
+package com.junit.basic;
+
+//import
+import java.util.ArrayList;
+
+//class
+public class ListManager {
+
+    //arraylist
+    private final ArrayList<Object> list;
+
+    //constructor
+    public ListManager(){
+        this.list = new ArrayList<>();
+    }
+
+    //add element method
+    public int addElement(int element) {
+        list.add(element);
+
+        return element;
+    }
+
+    //remove element method
+    public boolean removeElement(int element){
+
+        return list.remove((Integer) element);
+    }
+
+    //get size method
+    public int getSize(){
+        return list.size();
+    }
+
+}
