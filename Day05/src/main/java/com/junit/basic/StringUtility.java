8f8116d0d80544333fdea895b69e7b65e49869ef
Adarsh Gupta <adarshguptajhs@gmail.com>
2025-02-15 16:42:00 +0530

    Adarsh gupta - String Utility

---
+++ Day05/src/main/java/com/junit/basic/StringUtility.java
@@ -0,0 +1,34 @@
+//package
+package com.junit.basic;
+
+//class
+public class StringUtility {
+
+    //reverse method
+    public static StringBuilder reverse(StringBuilder str){
+
+        return str.reverse();
+
+    }
+
+    //is palindrome method
+    public static Boolean ispalindrome(StringBuilder sb){
+
+        StringBuilder sb1 = new StringBuilder(sb);
+
+        if (sb.equals(sb1.reverse())){
+            return true;
+        }
+
+        return false;
+
+    }
+
+    //to uppercase method
+    public static String toUppercase(String str){
+
+        return str.toUpperCase();
+
+    }
+
+}
